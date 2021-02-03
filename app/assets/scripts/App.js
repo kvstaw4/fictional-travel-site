d98c1c3fd51723d26a5163f4639f29e904114241
Kostas <kvstaw4@hotmail.com>
2021-02-03 13:48:38 +0200

    Completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,12 +4,29 @@ import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
 
+
 let mobileMenu = new MobileMenu();
 
  new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
  new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
  let stickyHeader = new StickyHeader();
+
+ let modal
+
+ document.querySelectorAll('.open-modal').forEach(el => {
+     el.addEventListener('click', e => {
+         e.preventDefault()
+         if (typeof modal == 'undefined') {
+            import('./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log('There was a problem'))
+         } else {
+            modal.openTheModal()
+         }
+     })
+ })
 
 
 if (module.hot) {
